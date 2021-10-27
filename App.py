23efe6b7459018b1714953825eac38e67cd6f397
Gabriel Sandoval <gabrielsandoval1020@gmail.com>
2021-10-28 02:00:36 +0800

    Add master counter to consider stamina constraint

--- App.py
+++ App.py
@@ -153,7 +153,9 @@ def grab_wave(sct):
 PREPARATION_LIMIT = 500
 WAVE_LIMIT = 100
 DROP_LIMIT = 50
+MASTER_COUNTER_LIMIT = 50
 
+master_counter = 0
 preparation_counter = 0
 wave_counter = 0
 drop_counter = 0
@@ -167,8 +169,12 @@ with mss() as sct:
     while "Screen capturing":
         last_time = time.time()
         
+        if master_counter >= MASTER_COUNTER_LIMIT:
+            print("MASTER_COUNTER_LIMIT reached. Exiting.")
+            exit()
+
         if state == "preparation":
-            print("[{}][{}%] Preparing for battle".format(state, round(preparation_counter/PREPARATION_LIMIT*100, 1)))
+            print("[{}][{}][{}%] Preparing for battle".format(master_counter, state, round(preparation_counter/PREPARATION_LIMIT*100, 1)))
             preparation_counter += 1
             
             if preparation_counter > PREPARATION_LIMIT:
@@ -184,11 +190,11 @@ with mss() as sct:
             wave_prediction = wave_model.predict(wave_scaler.transform([wave.reshape(height*width,)]))[0]
 
             if wave_prediction == 1:
-                print("[{}][{}%] WAVE 3 Detected".format(state, round(wave_counter/WAVE_LIMIT*100, 1)))
+                print("[{}][{}][{}%] WAVE 3 Detected".format(master_counter, state, round(wave_counter/WAVE_LIMIT*100, 1)))
                 wave_counter += 1
 
             if wave_counter > WAVE_LIMIT:
-                print("[{}][{}%] WAVE 3 Detected > {} times. Pressing 'ESC' now.".format(state, round(wave_counter/WAVE_LIMIT*100, 1), WAVE_LIMIT))
+                print("[{}][{}][{}%] WAVE 3 Detected > {} times. Pressing 'ESC' now.".format(master_counter, state, round(wave_counter/WAVE_LIMIT*100, 1), WAVE_LIMIT))
                 pyautogui.press('escape')
                 state = "menu"
                 wave_counter = 0
@@ -200,23 +206,25 @@ with mss() as sct:
             drop_prediction = drop_model.predict(drop_scaler.transform([drop.reshape(height*width,)]))[0]
 
             if drop_prediction == 1:
-                print("[{}][{}%] PURPLE DROP Detected.".format(state, round(drop_counter/DROP_LIMIT*100, 1)))
+                print("[{}][{}][{}%] PURPLE DROP Detected.".format(master_counter, state, round(drop_counter/DROP_LIMIT*100, 1)))
                 drop_counter += 1
 
             if drop_counter > DROP_LIMIT:
-                print("[{}][{}%] PURPLE DROP Detected > {} times.".format(state, round(drop_counter/DROP_LIMIT*100, 1), DROP_LIMIT))
+                print("[{}][{}][{}%] PURPLE DROP Detected > {} times.".format(master_counter, state, round(drop_counter/DROP_LIMIT*100, 1), DROP_LIMIT))
                 state == "battle"
                 drop_counter = 0
+                print("Found purple after {} tries".format(master_counter))
                 exit()
 
             if drop_prediction == 0:
-                print("[{}][{}%] NO PURPLE DROP".format(state, round(no_drop_counter/DROP_LIMIT*100, 1)))
+                print("[{}][{}][{}%] NO PURPLE DROP".format(master_counter, state, round(no_drop_counter/DROP_LIMIT*100, 1)))
                 no_drop_counter += 1
 
             if no_drop_counter > DROP_LIMIT:
-                print("[{}][{}%] NO PURPLE DROP. Quitting battle.".format(state, round(no_drop_counter/DROP_LIMIT*100, 1)))
+                print("[{}][{}][{}%] NO PURPLE DROP. Quitting battle.".format(master_counter, state, round(no_drop_counter/DROP_LIMIT*100, 1)))
                 pyautogui.click(x=930, y=775, interval=2)
                 pyautogui.click(x=930, y=775)
+                master_counter += 1
                 state = "preparation"
                 no_drop_counter = 0
                 drop_counter = 0
@@ -227,4 +235,3 @@ with mss() as sct:
         if cv2.waitKey(25) & 0xFF == ord("q"):
             cv2.destroyAllWindows()
             break
-
